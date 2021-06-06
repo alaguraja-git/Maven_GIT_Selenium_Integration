26c0cf140a412c6eb8059e1366a23a329fa4a039
alaguraja-git <alagu.dublin@gmail.com>
2021-06-06 07:52:49 +0530

    added Maven_Git_Selenium_Integration

---
+++ src/test/java/com/org/test.java
@@ -0,0 +1,41 @@
+package com.org;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class test {
+
+	WebDriver driver=null;
+	
+		@BeforeMethod
+		public void setUp()
+		{
+		System.setProperty("webdriver.chrome.driver","C:\\Workspace\\Maven_GIT_Selenium_Integration\\Browsers\\chromedriver.exe");  
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage");	
+		chromeOptions.setBinary("C:\\Users\\HP\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+		driver = new ChromeDriver(chromeOptions);			
+		}
+		
+		@Test
+		public void testcase1()
+		{
+			driver.get("https://www.google.com");	
+			String title = driver.getTitle();
+			System.out.println("title="+title);
+			
+		}
+		
+		@AfterMethod
+		public void closeDriver()
+		{
+			driver.close();
+		}
+		
+	}
+
